21b328ba2e5710606809b71f88d3589b1a849c16
andre-geraldes <andrejfgeraldes@gmail.com>
2015-03-16 16:25:32 +0000

    New files

---
+++ Trabalho/mainGerador.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+#include <string>
+#include <GL/glut.h>
+#include <math.h>
+
+
+
+int main(int argc, char **argv) {
+
+	if (argc > 1){
+
+
+	}
+	string nome;
+	cout << "Nome:\n";
+	getline(cin, nome);
+	cout << "O teu nome e " << nome << "\n";
+	
+	return 1;
+}
