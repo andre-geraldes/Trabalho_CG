a74a2877efe97c8ee244f81e27b20bb253530d5c
andre-geraldes <andrejfgeraldes@gmail.com>
2015-03-16 17:31:51 +0000

    Added plano

--- Trabalho/mainGerador.cpp
+++ Trabalho/mainGerador.cpp
@@ -5,7 +5,28 @@ using namespace std;
 #include <GL/glut.h>
 #include <math.h>
 
+void plano(float comp, float larg, int cmdh, int cmdv, string filename)
+{
+	float saltoz = comp / cmdv;
+	float saltox = larg / cmdh;
+	float refx = -(comp / 2);
+	float refz = -(larg / 2);
+	float i,j;
+	
+	for (i = refz; i < (larg/2); i += saltox)
+	{
+		for (j = refx; j < (comp/2); j += saltoz)
+		{
+			printf("%f 0 %f\n", i, j);
+			printf("%f 0 %f\n", i+saltox, j);
+			printf("%f 0 %f\n", i, j + saltoz);
 
+			printf("%f 0 %f\n", i, j + saltoz);
+			printf("%f 0 %f\n", i + saltox, j);
+			printf("%f 0 %f\n", i+saltox, j + saltoz);
+		}
+	}
+}
 
 int main(int argc, char **argv) {
 
@@ -13,10 +34,7 @@ int main(int argc, char **argv) {
 
 
 	}
-	string nome;
-	cout << "Nome:\n";
-	getline(cin, nome);
-	cout << "O teu nome e " << nome << "\n";
-	
+
+	plano(4, 2, 4, 2, "ola");
 	return 1;
 }
