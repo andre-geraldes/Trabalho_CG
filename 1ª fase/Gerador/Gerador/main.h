add398c280952fc821961d9302a7099565a4e4ac
Patrícia <bpatrcia@gmail.com>
2015-04-09 15:28:57 +0100

    Added fase2

---
+++ 1ª fase/Gerador/Gerador/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN
+#define _MAIN
+
+#define _USE_MATH_DEFINES
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+#endif
---
+++ 1ª fase/Motor3D/Motor3D/mainMotor3D.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN
+#define _MAIN
+
+#define _USE_MATH_DEFINES
+
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+using namespace std;
+
+//Estrutura ponto:
+struct Ponto
+{
+	double x;
+	double y;
+	double z;
+};
+
+#endif
