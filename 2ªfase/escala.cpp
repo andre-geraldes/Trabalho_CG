d8557c8b8d7ae339aa869c9275677a49ead147c8
Patrícia <bpatrcia@gmail.com>
2015-04-09 17:01:14 +0100

    classes +/-

---
+++ 2ªfase/escala.cpp
@@ -0,0 +1,14 @@
+Escala::Escala(){
+
+}
+Escala::Escala(){
+	x = 0.0;
+	y = 0.0;
+	z = 0.0;
+}
+
+Escala::Escala(float a, float b, float c){
+	x = a;
+	y = b;
+	z = c;
+}
---
+++ 2ªfase/primitiva.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <GL\glew.h>
+#include <GL\glut.h>
+
+class Primitiva{
+
+	string nome;
+	vector<Primitiva> filhos;
+	int cmdV;
+	int cmdH
+};
