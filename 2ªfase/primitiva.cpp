d34885a0a943c2624b946b43e97d1ad74c5b1102
Patrícia <bpatrcia@gmail.com>
2015-04-09 18:10:29 +0100

    Classes completed

---
+++ 2ªfase/primitiva.cpp
@@ -0,0 +1,17 @@
+#include "primitiva.h"
+
+
+Primitiva::Primitiva(){
+	nome = "";
+	transformacao = Transformacao::Transformacao();
+	filhos = vector<Primitiva>();
+	pontos = vector<Ponto>();
+}
+
+Primitiva::Primitiva(string n, vector<Primitiva> f, vector<Ponto> p, Transformacao t ){
+	nome = n;
+	filhos = f;
+	pontos = p;
+	transformacao = t;
+
+}
--- 2ªfase/primitiva.h
+++ 2ªfase/primitiva.h
@@ -6,33 +6,18 @@ class Primitiva{
 
 	string nome;
 	vector<Primitiva> filhos;
-	int cmdV;
-	int cmdH;
-	int cmdL;
-	float altura;
-	float largura;
-	float comprimento;
+	vector<Ponto> pontos;
 	Transformacao transformacao;
 
 public:
 	Primitiva();
-	Primitiva(string, vector<Primitiva>, int, int, int, float, float, float, Transformacao);
+	Primitiva(string, vector<Primitiva>, vector<Ponto>, Transformacao);
 	string getNome(){ return nome; }
+	vector<Ponto> getPontos() { return pontos; }
 	vector<Primitiva> getFilhos(){ return filhos; }
-	int getcmdV(){ return cmdV; }
-	int getcmdH(){ return cmdH; }
-	int getcmdL(){ return cmdL; }
-	float getAltura(){ return altura; }
-	float getLargura(){ return largura; }
-	float getComprimento(){ return comprimento; }
 	void setNome(string n){ nome = n; }
 	void setFilhos(vector<Primitiva> f){ filhos = f; }
-	void setcmdV(int v){ cmdV = v; }
-	void setcmdH(int h){ cmdH = h; }
-	void setcmdL(int l){ cmdL = l; }
-	void setAltura(float a){ altura = a; }
-	void setLargura(float l){ largura = l; }
-	void setComprimento(float c){ comprimento = c; }
+	void setPontos(vector<Pontos> p){ pontos = p; }
 	void setTransformacao(Transformacao t){ transformacao = t; }
 	virtual ~Primitiva();
 }; 
